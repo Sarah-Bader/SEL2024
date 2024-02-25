88683e609a097577608b4ec0bddc85958a04d5c6
SarahBaderr <160492937+SarahBaderr@users.noreply.github.com>
2024-02-25 12:06:17 +0100

    Add files via upload

---
+++ opdracht1/Installatie.ps1
@@ -0,0 +1,29 @@
+﻿# Installatie algemene applicaties
+Write-Host "Installatie algemene applicaties" -ForegroundColor Green
+
+# Adobe Acrobat Reader (of FoxIt Reader)
+choco install adobereader -y
+
+# Firefox
+choco install firefox -y
+
+# GitHub Desktop
+choco install github-desktop -y
+
+# Visual Studio Code
+choco install vscode -y
+
+# VLC Media Player
+choco install vlc -y
+
+# Installatie applicaties voor System Engineering Lab
+Write-Host "Installatie applicaties voor System Engineering Lab" -ForegroundColor Green
+
+# FileZilla
+choco install filezilla -y
+
+# VirtualBox
+choco install virtualbox -y
+
+# MySQL Workbench
+choco install mysql.workbench -y
